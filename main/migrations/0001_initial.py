bb4865b18e6555bed854cf5ae9a8f94811b7776b
gqdw <aca_jingru@qq.com>
2015-12-08 10:04:20 +0800

    基础commit

---
+++ main/migrations/0001_initial.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Book',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('book_name', models.CharField(max_length=40)),
+                ('time_buying', models.DateField(null=True, blank=True)),
+            ],
+        ),
+    ]
--- main/models.py
+++ main/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Book(models.Model):
+	book_name = models.CharField(max_length=40)
+	time_buying = models.DateField(null=True, blank=True)
+	def __unicode__(self):
+		return self.book_name
