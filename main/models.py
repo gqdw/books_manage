583c990fc22f97889598661fe325ab291178c9fe
gqdw <aca_jingru@qq.com>
2015-12-09 10:34:56 +0800

    add categorys class

--- main/models.py
+++ main/models.py
@@ -1,8 +1,19 @@
 from django.db import models
 
 # Create your models here.
+
+class Category(models.Model):
+	name = models.CharField(max_length=30)
+
+	def __unicode__(self):
+		return self.name
+
+
 class Book(models.Model):
 	book_name = models.CharField(max_length=40)
 	time_buying = models.DateField(null=True, blank=True)
+	category = models.ManyToManyField(Category)
+
 	def __unicode__(self):
 		return self.book_name
+		# return "%s %s" % (self.book_name, self.category.name)
