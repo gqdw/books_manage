d3f409239604dd199fdb5bb9377d4d2f7310f5d8
gqdw <aca_jingru@qq.com>
2016-06-19 17:41:09 +0800

    change view.index

--- books_manage/urls.py
+++ books_manage/urls.py
@@ -18,6 +18,6 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
-	url(r'^$','main.views.Index'),
+	url(r'^$','main.views.booklist'),
 	url(r'^booklist','main.views.booklist'),
 ]
