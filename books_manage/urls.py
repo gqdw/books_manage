e183d6d81fcfe8c5747040e1654011917a81a7e6
gqdw <aca_jingru@qq.com>
2016-06-19 17:26:30 +0800

    add booklist view, template

--- books_manage/urls.py
+++ books_manage/urls.py
@@ -19,4 +19,5 @@ from django.contrib import admin
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
 	url(r'^$','main.views.Index'),
+	url(r'^booklist','main.views.booklist'),
 ]
--- main/views.py
+++ main/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from main.models import Book
 # Create your views here.
 
 def Index(request):
 	return HttpResponse('ok')
+
+def booklist(request):
+	books = Book.objects.all()
+	return render(request, 'booklist.html', {'books':books})
